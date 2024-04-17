3c1b3b367aca0177cb0ba2533a8c0f77a7ce8906
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-17 19:15:31 +0500

    This is word coyunter project

---
+++ index.ts
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+console.log("Welcome to Word Counter With MehwishNaz")
+const ans :{ sent : string;} = await inquirer.prompt
+([
+{
+  name: "sent",
+  type: "input",
+  message: "Enter your sentence to count the word:"
+}
+])
+const words = ans.sent.trim().split(" ")
+console.log(words)
+console.log(`Your sentence word count is: ${words.length}`)
